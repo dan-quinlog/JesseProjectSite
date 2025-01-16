02cab23d98fb1c1e1f58b38b8d4a34ab4e0ebf50
Daniel <dan.quinlog@gmail.com>
2025-01-15 22:32:51 -0700

    updated to include simple react front page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Coming Soon</h1>
+        <p>My Project Portfolio Site</p>
+        <p>Under Construction</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
